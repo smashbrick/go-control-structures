278ecda67427140b7f5b00ea444dfc91d09c5e26
Smashbrick <smashbrick@yahoo.com>
2024-10-14 16:31:55 +0530

    Added file reading and writing

--- bank.go
+++ bank.go
@@ -1,11 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
+
+func getBalanceFromFile() float64{
+	data, _ := os.ReadFile(accountBalanceFile, )
+	balanceText := string(data)
+  balance, _ := strconv.ParseFloat(balanceText, 64)
+	return balance
+}
+
+func writeBalanceToFile(balance float64){
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644 )
+}
 
 func main() {
-var accountBalance = 1000.0
+var accountBalance = getBalanceFromFile()
 
-	fmt.Println("Welcome to Go Bank!")
+fmt.Println("Welcome to Go Bank!")
+
+for  {
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
 	fmt.Println("2. Deposit money")
@@ -25,16 +45,46 @@ if choice == 1 {
 	fmt.Print("Your deposit: ")
 	var depositAmount float64 
 	fmt.Scan(&depositAmount)
+
+	if depositAmount <= 0 {
+		fmt.Println("Invalid amount. Must be greater than 0")
+		// return 
+		continue 
+
+	}
 	accountBalance +=  depositAmount
 	fmt.Println("Balance updated! New amount:", accountBalance)
+	writeBalanceToFile(accountBalance)
 } else if choice == 3 {
 	fmt.Print("Withdrawal amount: ")
 	var withdrawalAmount float64 
 	fmt.Scan(&withdrawalAmount)
+	if withdrawalAmount <= 0 {
+		fmt.Println("Invalid amount. Must be greater than 0.")
+continue
+	} else if withdrawalAmount > accountBalance {
+			fmt.Println("Invalid amount. You can't withdraw more than you have.")
+			return
+		
+	}
 	accountBalance -=  withdrawalAmount
 	fmt.Println("Balance updated! New amount:", accountBalance)
+	writeBalanceToFile(accountBalance)
+
+} else {
+fmt.Println("Goodbye!")
+// return
+break
 }
 
-fmt.Println("Your choice", choice)
+
 }
+fmt.Println("Thanks for choosing our bank")
+
+
+}
+
+
+
+
 
